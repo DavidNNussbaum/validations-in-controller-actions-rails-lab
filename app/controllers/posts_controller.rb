de6277667079bd72e9f5524868766eb338e464e4
DavidNNussbaum <brocknynj@aol.com>
2021-03-06 22:07:42 -0500

    commit tree 360a3bb698e15c86d77ae07173bf741c53a9c394

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,10 +7,25 @@ class PostsController < ApplicationController
   def edit
   end
 
-  def update
-    @post.update(post_params)
+  def new
+  end
 
-    redirect_to post_path(@post)
+  def create
+    @post = Post.new(author_params)
+    if @post.valid?
+       @post.save
+       redirect_to post_path(@post)
+    else
+      render :new
+    end 
+  end
+
+  def update
+    if @post.update(post_params)
+       redirect_to post_path(@post)
+    else
+       render :edit
+    end
   end
 
   private
